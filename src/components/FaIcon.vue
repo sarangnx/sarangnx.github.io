f5cd6db1fa7afcfb60d50734e1e6d10f28dc8460
Sarang <sarang98divakaran@gmail.com>
2020-08-25 19:10:18 +0530

    Font Awesome icon component

---
+++ src/components/FaIcon.vue
@@ -0,0 +1,38 @@
+<template>
+    <span :class="[type, iconClass]"></span>
+</template>
+
+<script>
+export default {
+    props: {
+        icon: {
+            type: String
+        },
+        regular: {
+            type: Boolean
+        },
+        solid: {
+            type: Boolean
+        },
+        brand: {
+            type: Boolean
+        }
+    },
+    computed: {
+        type() {
+            const type = this.regular
+                ? 'far'
+                : this.solid
+                ? 'fas'
+                : this.brand
+                ? 'fab'
+                : 'fas';
+
+            return type;
+        },
+        iconClass() {
+            return `fa-${this.icon}`;
+        }
+    }
+};
+</script>
