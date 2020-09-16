18def607675632e15a105a912c27062b046360cd
Sarang <sarang98divakaran@gmail.com>
2020-09-16 17:34:04 +0530

    routes with baselayout and other routes as children

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,24 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Home from '../views/Home.vue';
 
 const routes = [
     {
         path: '/',
-        name: 'Home',
-        component: Home
-    },
-    {
-        path: '/about',
-        name: 'About',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
         component: () =>
-            import(/* webpackChunkName: "about" */ '../views/About.vue')
+            import(/* webpackChunkName: "layout" */ '@/layouts/BaseLayout'),
+        children: [
+            {
+                path: '',
+                name: 'Home',
+                component: () =>
+                    import(/* webpackChunkName: "home" */ '@/views/Home')
+            },
+            {
+                path: '/about',
+                name: 'About',
+                component: () =>
+                    import(/* webpackChunkName: "about" */ '../views/About.vue')
+            }
+        ]
     }
 ];
 
