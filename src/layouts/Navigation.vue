dcd39ced12194d480f37a0d1c32e2d2cccb3af31
Sarang <sarang98divakaran@gmail.com>
2020-09-16 17:30:49 +0530

    navigation bar component

---
+++ src/layouts/Navigation.vue
@@ -0,0 +1,35 @@
+<template>
+    <nav
+        class="top-nav px-3 py-4 d-flex justify-content-between align-items-center"
+    >
+        <div class="nav-logo">
+            <img src="/img/sg.svg" />
+        </div>
+        <div class="links">
+            <router-link class="px-2" to="/about">About</router-link>
+            <router-link class="px-2" to="/about">Skills</router-link>
+            <router-link class="px-2" to="/about">Works</router-link>
+            <router-link class="px-2" to="/about">Contact</router-link>
+        </div>
+    </nav>
+</template>
+
+<style lang="scss">
+.nav-logo {
+    img {
+        height: calc(1em + 1.5vw);
+        width: auto;
+        min-height: 25px;
+    }
+}
+
+.top-nav {
+    background: var(--nav-bg);
+
+    .links a {
+        text-decoration: none;
+        color: white;
+        font-family: 'Roboto Mono';
+    }
+}
+</style>
