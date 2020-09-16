a956f45540c0582f5b2bf8d59ad9d03bb4099aa5
Sarang <sarang98divakaran@gmail.com>
2020-09-16 17:31:08 +0530

    BaseLayout

---
+++ src/layouts/BaseLayout.vue
@@ -0,0 +1,16 @@
+<template>
+    <navigation />
+    <main>
+        <router-view />
+    </main>
+</template>
+
+<script>
+import Navigation from './Navigation';
+
+export default {
+    components: {
+        Navigation
+    }
+};
+</script>
