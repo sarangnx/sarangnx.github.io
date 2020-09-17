197e62a40e5d0011469c8048c81f0f4e6331edca
Sarang <sarang98divakaran@gmail.com>
2020-09-17 21:50:26 +0530

    added bottom nav component to app

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,14 @@
 <template>
     <router-view />
+    <bottom-nav v-if="!$bp.smAndUp"></bottom-nav>
 </template>
+
+<script>
+import BottomNav from '@/components/BottomNav.vue';
+
+export default {
+    components: {
+        BottomNav
+    }
+};
+</script>
