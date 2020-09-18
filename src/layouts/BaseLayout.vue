77caefbef43143f344fe69f18a3240ea4275f156
Sarang <sarang98divakaran@gmail.com>
2020-09-18 16:01:23 +0530

    height is 60px not 50px

--- src/layouts/BaseLayout.vue
+++ src/layouts/BaseLayout.vue
@@ -17,12 +17,12 @@ export default {
 
 <style lang="scss">
 .mobile-view {
-    // 50px = height of bottom nav
+    // 60px = height of bottom nav
     // 3rem = py-4 in top nav
     // max(26px, calc(1rem + 1.5vw)) = height of logo
-    height: calc(100% - 50px - 3rem - max(26px, calc(1rem + 1.5vw)));
+    height: calc(100% - 60px - 3rem - max(26px, calc(1rem + 1.5vw)));
     > div:last-child {
-        margin-bottom: 50px;
+        margin-bottom: 60px;
     }
 }
 </style>
