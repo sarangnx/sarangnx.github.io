fb345f85596ef506ac7b31f151ae93dbab4bd263
Sarang <sarang98divakaran@gmail.com>
2020-09-18 16:09:41 +0530

    add background

--- src/views/About.vue
+++ src/views/About.vue
@@ -10,5 +10,25 @@
     h1 {
         font-weight: bold;
     }
+    &::before {
+        content: '';
+        background: url('/img/curvybg-1.svg');
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        background-repeat: no-repeat;
+        left: 0px;
+        top: 0px;
+        background-position: bottom;
+        background-size: contain;
+        z-index: -1;
+    }
+}
+
+// to keep svg above bottom nav in mobile-view
+.mobile-view {
+    .about::before {
+        background-position: 0px calc(100% - 60px);
+    }
 }
 </style>
