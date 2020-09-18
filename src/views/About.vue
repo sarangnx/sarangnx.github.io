cdc882ccbff87c67fc8650fffe8c9e30b1e45c5b
Sarang <sarang98divakaran@gmail.com>
2020-09-18 17:00:13 +0530

    about me text added

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,25 @@
 <template>
-    <div class="about container mt-5">
+    <div class="about col-12 col-sm-8 col-lg-5 mt-5 mx-auto">
         <h1>About Me</h1>
-        <p></p>
+        <p>
+            I am a full stack developer, who started out in php. I am a person
+            who loves javascript despite issues like -
+            <span class="code">0.1 + 0.2 = 0.30000000000000004</span>,
+            <span class="code">'1' + 1 = '11'</span> but
+            <span class="code">'1' - 1 = 0</span>,
+            <span class="code">'b' + 'a' ++ 'a' + 'a' = 'baNaNa'</span> and
+            numerous other crazy issues that are not in any other languages.
+            When I heard about writing server side code with JS, I ditched php
+            for good and took on Node.js.
+        </p>
+        <p>
+            Being a CS graduate who pushed himself to learn more about new
+            technologies, I am a “Jack of All Trades and Master of Some”.
+        </p>
+        <p>
+            Apart from coding all day and night, anime, games and memes are what
+            touches my soul.
+        </p>
     </div>
 </template>
 
@@ -13,7 +31,7 @@
     &::before {
         content: '';
         background: url('/img/curvybg-1.svg');
-        position: absolute;
+        position: fixed;
         width: 100%;
         height: 100%;
         background-repeat: no-repeat;
@@ -22,6 +40,15 @@
         background-position: bottom;
         background-size: contain;
         z-index: -1;
+    }
+
+    p {
+        line-height: 2em;
+        span.code {
+            white-space: nowrap;
+            background: #313131;
+            padding: 0.1em 0.4em;
+        }
     }
 }
 
