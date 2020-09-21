5063022ac36395576821f005a2ac38c8ab6c5c34
Sarang <sarang98divakaran@gmail.com>
2020-09-21 16:06:16 +0530

    calculate initial vh for mobile

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,12 @@ import BottomNav from '@/components/BottomNav.vue';
 export default {
     components: {
         BottomNav
+    },
+    mounted() {
+        // set --vh property in root to fix issues with mobile vh
+        // this calculates vh excluding address bar.
+        let vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
     }
 };
 </script>
