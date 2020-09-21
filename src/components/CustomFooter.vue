7c8a32adeea098b305e4df422b00b4768dfd9a3d
Sarang <sarang98divakaran@gmail.com>
2020-09-21 15:19:41 +0530

    created component footer

---
+++ src/components/CustomFooter.vue
@@ -0,0 +1,12 @@
+<template>
+    <footer :class="{ 'mobile-view': !$bp.smAndUp }"></footer>
+</template>
+
+<style lang="scss">
+footer {
+    padding: 20px;
+    &.mobile-view {
+        margin-bottom: 60px !important;
+    }
+}
+</style>
