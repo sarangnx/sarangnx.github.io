5d13222b71b12a8e8629566774004218df7c16fa
Sarang <sarang98divakaran@gmail.com>
2020-09-28 14:50:50 +0530

    added pageloading component

---
+++ src/components/PageLoading.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="page-loading">
+        <div class="looping-rhombuses-spinner">
+            <div class="rhombus"></div>
+            <div class="rhombus"></div>
+            <div class="rhombus"></div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss">
+#page-loading {
+    position: fixed;
+    top: 0px;
+    left: 0px;
+    z-index: 999;
+    height: 100%;
+    width: 100%;
+    background: black;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
