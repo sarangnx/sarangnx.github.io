1d5f577d87d8c86cf8023ddc421b1346f69a5450
Sarang <sarang98divakaran@gmail.com>
2020-09-29 12:34:39 +0530

    added contact form

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -2,35 +2,53 @@
     <div class="curvy-bg">
         <div class="contact col-12 col-sm-10 col-lg-8 pt-5 mx-auto">
             <h1>Contact</h1>
-            <div class="mt-5">
-                <div class="contacts">
-                    <a href="mailto:sarangnkd@gmail.com" @click="copyToClipboard('sarangnkd@gmail.com', 'email')">
-                        <fa-icon icon="at" type="fas" />
-                        <span class="ml-3">sarangnkd@gmail.com</span>
-                    </a>
-                    <a href="https://www.linkedin.com/in/sarang-n-270756170/" target="_blank">
-                        <fa-icon icon="linkedin" type="fab" />
-                        <span class="ml-3">Sarang N</span>
-                    </a>
-                    <a href="#" @click.prevent="copyToClipboard('itachi#8350', 'discord')">
-                        <fa-icon icon="discord" type="fab" />
-                        <span class="ml-3">itachi#8350</span>
-                    </a>
+            <div class="d-flex flex-column flex-md-row">
+                <div class="mt-5 col-12 col-md-6 p-0">
+                    <div class="contacts mb-5">
+                        <a href="mailto:sarangnkd@gmail.com" @click="copyToClipboard('sarangnkd@gmail.com', 'email')">
+                            <fa-icon icon="at" type="fas" />
+                            <span class="ml-3">sarangnkd@gmail.com</span>
+                        </a>
+                        <a href="https://www.linkedin.com/in/sarang-n-270756170/" target="_blank">
+                            <fa-icon icon="linkedin" type="fab" />
+                            <span class="ml-3">Sarang N</span>
+                        </a>
+                        <a href="#" @click.prevent="copyToClipboard('itachi#8350', 'discord')">
+                            <fa-icon icon="discord" type="fab" />
+                            <span class="ml-3">itachi#8350</span>
+                        </a>
+                    </div>
+                    <div class="contacts mt-5">
+                        <h5 class="font-weight-bold">Profiles</h5>
+                        <a href="https://github.com/sarangnx" target="_blank">
+                            <fa-icon icon="github" type="fab" />
+                            <span class="ml-3">sarangnx</span>
+                        </a>
+                        <a href="https://gitlab.com/sarangnx" target="_blank">
+                            <fa-icon icon="gitlab" type="fab" />
+                            <span class="ml-3">sarangnx</span>
+                        </a>
+                        <a href="https://stackoverflow.com/users/9155456/srx" target="_blank">
+                            <fa-icon icon="stack-overflow" type="fab" />
+                            <span class="ml-3">user:9155456 (srx)</span>
+                        </a>
+                    </div>
                 </div>
-                <div class="contacts mt-5">
-                    <h5 class="font-weight-bold">Profiles</h5>
-                    <a href="https://github.com/sarangnx" target="_blank">
-                        <fa-icon icon="github" type="fab" />
-                        <span class="ml-3">sarangnx</span>
-                    </a>
-                    <a href="https://gitlab.com/sarangnx" target="_blank">
-                        <fa-icon icon="gitlab" type="fab" />
-                        <span class="ml-3">sarangnx</span>
-                    </a>
-                    <a href="https://stackoverflow.com/users/9155456/srx" target="_blank">
-                        <fa-icon icon="stack-overflow" type="fab" />
-                        <span class="ml-3">user:9155456 (srx)</span>
-                    </a>
+                <div class="col-12 col-md-6 p-0 contact-form-wrapper">
+                    <div id="contact-form">
+                        <div class="form--group">
+                            <input type="text" id="name" class="form--field" />
+                            <label for="name" class="form--label">Name</label>
+                        </div>
+                        <div class="form--group">
+                            <input type="email" id="email" class="form--field" />
+                            <label for="email" class="form--label">Email</label>
+                        </div>
+                        <div class="form--group">
+                            <textarea rows="4" id="message" class="form--field"></textarea>
+                            <label for="message" class="form--label">Message</label>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -82,6 +100,58 @@ export default {
                 color: #cacaca;
             }
         }
+    }
+}
+#contact-form {
+    display: flex;
+    flex-direction: column;
+    background: #1f1f1f;
+    padding: 2rem;
+    border-radius: 10px;
+    box-shadow: 4px 4px 4px #0b0b0b70;
+
+    .form--group {
+        position: relative;
+        padding: 15px 0px;
+    }
+    .form--field {
+        width: 100%;
+        border: 0px;
+        font-size: 0.9rem;
+        padding: 1rem;
+        outline: 0px;
+        background: #0f0f0f;
+        color: #ffffff;
+        border-radius: 10px;
+        box-shadow: 4px 4px 4px rgba(0, 0, 0, 0.2);
+    }
+    .form--label {
+        position: absolute;
+        left: 0px;
+        top: 0px;
+        font-weight: bold;
+        margin: 0px;
+        font-size: 0.9rem;
+        left: 20px;
+        top: 0.2rem;
+        color: #b5b5b5;
+    }
+    textarea {
+        resize: none;
+    }
+}
+.contact-form-wrapper {
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+}
+.mobile-view {
+    .contact-form-wrapper {
+        justify-content: center;
+        margin-top: 7rem;
+    }
+    #contact-form {
+        width: 100%;
     }
 }
 </style>
