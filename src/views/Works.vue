a525618c20a865a61f8f592ea3013cd1f8ebd472
Sarang <sarang98divakaran@gmail.com>
2020-11-07 11:52:40 +0530

    🌟 feat: added new work: black pearl

--- src/views/Works.vue
+++ src/views/Works.vue
@@ -4,6 +4,19 @@
             <h1>Works</h1>
             <div class="d-flex flex-column">
                 <work
+                    image="/img/projects/blackpearl.png"
+                    title="Black Pearl - Download Torrents to Google Drive"
+                    :tech="['socket.io', 'vue.js', 'express.js', 'googleapis', 'heroku']"
+                    source="https://github.com/sarangnx/torrent-webapp"
+                    website="https://torrent.srng.dev/"
+                >
+                    <template #description>
+                        This service helps you to download any torrents directly to Google Drive. Like other torrent
+                        clients such as µTorrent you can select and download partial torrents too. You can donwload a
+                        single file or a complete folder. And all of this works without using any of your bandwidth.
+                    </template>
+                </work>
+                <work
                     image="/img/projects/bifrost.png"
                     title="Bifrost - Save to Google Drive"
                     :tech="['nuxt.js', 'express.js', 'googleapis', 'heroku']"
